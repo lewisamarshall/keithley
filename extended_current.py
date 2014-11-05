ee5f33d5f3aedb7de03532203b39415a039818f0
Lewis A. Marshall <lewis@purigenbio.com>
2014-11-05 15:17:27 -0800

    value changes.

--- extended_current.py
+++ extended_current.py
@@ -1,24 +1,24 @@
 import keithley
 
-file = 'itp.csv'
+file = './C1_lysate_exp1.2.csv'
 
 k = keithley.Keithley()
 k.write(':SOUR:FUNC:MODE CURR')
 k.write(':SOUR:CURR:MODE FIX')
 k.write(':SENS:VOLT:RANG 1000')
 k.write(':SENS:VOLT:RANG:AUTO 1')
-k.write(':SENS:VOLT:PROT 1000')
+k.write(':SENS:VOLT:PROT 1100')
 k.write(':SENS:CURR:PROT 1')
 k.write(':SENS:VOLT:NPLC 1')
 k.write(':SOUR:CLE:AUTO OFF')
 k.write(':SOUR:CURR:RANG .01')
 k.write(':SOUR:CURR:RANG:AUTO 1')
 
-current = 0.0005
+current = 0.0008
 k.set_i(current)
 
 k.output('ON')
-k.capture(2000, file, 'wb')
+k.capture(2000, file, 'ab')
 k.show_gui()
 k.stop_capture()
 k.output('OFF')
