e3ae6d877c5a3c130e6ffbe37970f2988fd328b6
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-11-13 11:35:23 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='keithley',
+      version='0.1.0',
+      author='Lewis A. Marshall',
+      author_email='lewis@purigenbio.com',
+      url='https://github.com/lewisamarshall/keithley',
+      use_2to3=True,
+      description='A Python package for analyzing Purigen data files.',
+      packages=find_packages(),
+      requires=['serial'],
+      package_data={}
+      )
